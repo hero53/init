71fe7bd4fd055c34825ca089c8a4b4e034f6c351
hacker <profseur@.com>
2023-11-28 22:26:05 +0000

    push

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 
 <template>
-<HellowordVue :nom="nom"/>
+  <!-- <HellowordVue :user="user" /> -->
+  <HellowordVue/>
+  <div class="text-green">
+    {{ user.name }}
+  </div>
 </template>
 
 
 <script setup>
 import HellowordVue from './components/Helloword.vue';
+import { reactive } from 'vue';
 
-const nom = "Israël";
+const user = reactive({ name: "Israël", level: 6 });
+
+const display = () => {
+  console.log(user.name, user.level);
+}
 </script>
 
 <style scoped>
+.text-red{
+    color: red;
+}
+
+.text-green{
+    color: green;
+}
 </style>
--- src/components/Helloword.vue
+++ src/components/Helloword.vue
@@ -1,12 +1,22 @@
 
 <template>
-    <h1>hello word {{props.nom}}</h1>
-    </template>
+    <h1>hello word</h1>
     
-    <script setup>
-    const props = defineProps(['nom'])
-    </script>
+    <!-- <p>{{ props.user.name  }}</p>
+    <p>{{ props.user.level }}</p> -->
+</template>
     
-    <style scoped>
-    </style>
+<script setup>
+// const props = defineProps(['user'])
+</script>
+    
+<style scoped>
+.text-red{
+    color: red;
+}
+
+.text-green{
+    color: green;
+}
+</style>
     
