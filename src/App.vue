23f53e0aac46252764157fbcef10ab603c787694
hacker <profseur@.com>
2023-11-28 15:25:38 +0000

    vue js learning first push

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+
+<template>
+<HellowordVue :nom="nom"/>
+</template>
+
+
+<script setup>
+import HellowordVue from './components/Helloword.vue';
+
+const nom = "Israël";
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Helloword.vue
@@ -0,0 +1,12 @@
+
+<template>
+    <h1>hello word {{props.nom}}</h1>
+    </template>
+    
+    <script setup>
+    const props = defineProps(['nom'])
+    </script>
+    
+    <style scoped>
+    </style>
+    
